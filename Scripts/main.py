2ca43ae8a656ceb364768e1972518b3312e5c48b
Henrique Abreu <henrique.abreu@fujitsu.com>
2023-12-26 22:11:56 +0000

    sd

--- Scripts/main.py
+++ Scripts/main.py
@@ -99,17 +99,16 @@ if __name__ == "__main__":
     targetDirectory = os.getcwd() + "/"
 
     # Step 1: Verify Folders
-    folders = ['Extract', 'JsonConverted', 'Output', 'Reports']
+    folders = ['Extract', 'JsonConverted', 'Output']
 
     for folder in folders:
         InOut.existsFolder(targetDirectory, folder)
 
     # Step 2: Unzip Report
     extracts(targetDirectory)
-
+    '''
     # Step 3: Convert to Json
     JsonConverted(targetDirectory)
-    '''
     # Step 4: Report find Dataset
     elements = ["Sprint", "Issues", "Dog"]
     finder(targetDirectory, elements)
